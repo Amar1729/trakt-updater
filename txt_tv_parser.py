bbd5090cf54e106a419ba76715f9ec8b1ff69c7f
Amar1729 <amar.paul16@gmail.com>
2021-03-14 15:39:27 -0400

    add comments for txt_tv_parser

--- txt_tv_parser.py
+++ txt_tv_parser.py
@@ -1,9 +1,30 @@
 #! /usr/bin/env python3
 
 """
+This file is required for this script to run:
 fname = "./tv-shows-by-date-full.txt"
 # content copied from:
+# (can just ctrl-a and copy all of it, then delete first few/last few lines)
 # https://en.wikipedia.org/wiki/List_of_American_television_programs_by_debut_date
+
+# format of the file should look something like:
+
+```
+2010
+January
+
+    January 3 – Frank the Entertainer in a Basement Affair
+    January 3 – Worst Cooks in America
+
+February
+
+    Feb n - ...
+
+2011
+January
+
+    January n - ...
+```
 
 # this script parses the output and yields each line one at a time, prefixed by its release year
 # e.g.
@@ -11,6 +32,7 @@ fname = "./tv-shows-by-date-full.txt"
 """
 
 import re
+import os
 
 
 fname = "./tv-shows-by-date-full.txt"
@@ -18,6 +40,9 @@ fname = "./tv-shows-by-date-full.txt"
 
 def find_movies(limit=0):
     year = 0
+
+    if not os.path.exists(fname):
+        raise Exception("Read docstring for txt_tv_parser.py")
 
     counter = 0
     with open(fname) as f:
